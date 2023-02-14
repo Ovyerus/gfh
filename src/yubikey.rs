28dc1d19d5e2769ff19f53aaadd22be4866391ea
Duale Siad <me@wale.id.au>
2023-02-14 14:54:02 +1100

    Fix YubiKey detection when user has multiple smartcard readers

--- src/yubikey.rs
+++ src/yubikey.rs
@@ -4,15 +4,17 @@ use yubikey_api::Context as YKContext;
 use crate::util::FidoDevice;
 
 pub fn get_yubikeys() -> Result<Vec<FidoDevice>> {
-    let mut readers =
+    let mut readers: YKContext =
         YKContext::open().with_context(|| "failed to create reader context for yubikeys")?;
     let mut output = Vec::<FidoDevice>::new();
 
     for reader in readers.iter()? {
-        let yubikey = reader
-            .open()
-            .with_context(|| format!("failed to open yubikey {}", reader.name()))?;
-        output.push(FidoDevice::YubiKey(yubikey))
+        if reader.name().as_ref().to_ascii_lowercase().contains(&"yubikey") {
+            let yubikey = reader
+                .open()
+                .with_context(|| format!("failed to open yubikey {}", reader.name()))?;
+            output.push(FidoDevice::YubiKey(yubikey))   
+        }
     }
 
     Ok(output)
